8fb0177f184d8bdae38d9e510310d0fc94425abe
Brian Scoles <brianscoles@gmail.com>
2014-12-01 14:53:22 -0800

    add local angular.js.  add basic task add, view, and archive.

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
-var app = angular.module('Todoloo', [])
+(function(){
+  console.log("initializing app.js");
+  var tasks = ["get milk","drink milk","get more milk"];
+  var archivedTasks = [];
+  var app = angular.module('Todoloo', [])
 
-.factory($scope, function(){
+.factory('todoFactory', function($scope){
+    //helper functions and logic here
+  })
 
-})
+.controller('formController', function($scope){
+    $scope.addTask = function(){
+      console.log("new task submitted!");
+      tasks.push( $scope.task )
+      $scope.task = "";
+    };
+  })
 
-.controller('TodoController',function($scope){
+  // a controller for each item individually
+  .controller('taskController',function($scope){
+    $scope.closeTask = function(task){
 
-});
+      //find task in array, splice it out, and push it to archivedTasks
+      archivedTasks.push(tasks.splice(tasks.indexOf(task),1)[0]);
+    }
+  })
+
+  //a controller for the whole view panel of tasks
+  .controller('panelController',function($scope){
+    $scope.tasks = tasks;
+  })
+
+})();
